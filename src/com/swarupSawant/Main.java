ef5620ffdc1eb352564abd3afc5540f8923fd450
Swarup Sawant <swarupsawant2005@gmail.com>
2025-12-07 10:46:42 +0530

    a poor solution without using generics.

--- src/com/swarupSawant/Main.java
+++ src/com/swarupSawant/Main.java
@@ -3,10 +3,15 @@ package com.swarupSawant;
 import com.swarupSawant.exceptions.ExceptionsDemo;
 import com.swarupSawant.generics.GenericList;
 import com.swarupSawant.generics.List;
+import com.swarupSawant.generics.User;
 
 public class Main {
     public static void main(String[] args) {
        var list = new List();
        list.add(1);
+       list.add("1");
+       list.add(new User());
+
+       int number = (int)list.get(0);
     }
 }
--- src/com/swarupSawant/generics/User.java
+++ src/com/swarupSawant/generics/User.java
@@ -1,5 +1,6 @@
 package com.swarupSawant.generics;
 
 public class User {
-
+    private User[] items = new User[10];
+    private int count;
 }
